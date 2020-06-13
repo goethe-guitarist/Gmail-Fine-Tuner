8dc67e23ac2245e585a49affc2af67554eeab668
Jeff Moeller <jeff.e.moeller@gmail.com>
2020-06-13 10:37:02 -0500

    Worked out initial plumbing for json configuration.

--- ConsoleCoreA/Program.cs
+++ ConsoleCoreA/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace ConsoleCoreA
 {
@@ -6,7 +10,46 @@ namespace ConsoleCoreA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var x = config.GetChildren();
+
+            Console.WriteLine($"FilePath is { config["Input File Path"] } !");
+
+            //// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
+            //// The files used in this example are created in the topic
+            //// How to: Write to a Text File. You can change the path and
+            //// file name to substitute text files of your own.
+
+            //// Example #1
+            //// Read the file as one string.
+            //string text = System.IO.File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+
+            //// Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            //// Example #2
+            //// Read each line of the file into a string array. Each element
+            //// of the array is one line of the file.
+            //string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+
+            //// Display the file contents by using a foreach loop.
+            //System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            //foreach (string line in lines)
+            //{
+            //    // Use a tab to indent each line of the file.
+            //    Console.WriteLine("\t" + line);
+            //}
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
     }
 }
